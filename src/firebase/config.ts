3af26cf80a0333b94e19642e5698197eb3d11f5f
JOSE RAMON TAPIA GOTT <utp0155269@alumno.utpuebla.edu.mx>
2025-10-23 22:22:48 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4838645972-8af29",
+  "appId": "1:846867603181:web:68a002e1ae31becb8936e4",
+  "apiKey": "AIzaSyDkj2T5BTStPuZKvd6Y6kdiLntcLoxEFoU",
+  "authDomain": "studio-4838645972-8af29.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "846867603181"
+};
